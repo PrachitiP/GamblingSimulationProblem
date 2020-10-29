41e214d149c02034b89e2919bc6f2ec9719b38cf
PrachitiP <patilprachiti35@gmail.com>
2020-10-29 11:35:36 +0530

    Stake and Bet Value

---
+++ UC1_StakeBet.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class UC1_StakeBet {
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Gambling Simulation Problem");
+		int stake = 100;
+		int bet = 1;
+		System.out.println("Stake: " + stake + "\nBet: " + bet);
+	}
+}
